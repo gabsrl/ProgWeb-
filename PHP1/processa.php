2a6718750a8f903829ade84e1f0f51847e7f4674
Gabriel Lima <grl@icomp.ufam.edu.br>
2018-05-08 21:02:24 -0400

    Primeiro exercicio de PHP. Formulario e metodo GET

---
+++ PHP1/processa.php
@@ -0,0 +1,7 @@
+<?php
+		echo '<h1>Contato - ' . $_GET['id_nome'] . '</h1>'; 
+		echo '<p>Nome: ' . $_GET['id_nome'] .'</p>'; 
+		echo '<p>Email: '. $_GET['id_email'] . '</p>';
+		echo '<p>Website: ' . $_GET['id_website'] . '</p>';
+		echo '<p>Mensagem: ' . $_GET['id_mensagem'] . '</p>';
+?>
