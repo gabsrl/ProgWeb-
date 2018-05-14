d3894c53e14515b91c332e1be2ddb02037fe9351
Gabriel Lima <grl@icomp.ufam.edu.br>
2018-05-14 00:06:27 -0400

    Exercício 2 de JS: Jokenpo. Obs: Aperte F12 antes de começar a jogar para abrir o console do browse.

---
+++ JS2/jokenpo.js
@@ -0,0 +1,42 @@
+function cpuJoga() { 
+	var x = Math.floor((Math.random()*3) + 1);  
+	return x; 
+}
+
+jogadorPerdeu = false;
+rodada = 0;
+dicionario = []; 
+dicionario[1] = "papel"; 
+dicionario[2] = "pedra"; 
+dicionario[3] = "tesoura"; 
+
+
+while(jogadorPerdeu == false) { 
+	var jogadaPlayer = parseInt(prompt("Escolha sua jogada:\n 1 - Papel\n2 - Pedra\n3 - Tesoura"));
+	var jogadaCpu = cpuJoga(); 	
+	if(jogadaPlayer == 1 && jogadaCpu == 2) { //player joga papel e cpu joga pedra 
+		console.log("O computador jogou pedra\nVoce ganhou!"); 
+		rodada++; 
+	}
+
+	else if(jogadaPlayer == 2 && jogadaCpu == 3) {  //player joga pedra e cpu joga tesoura
+		console.log("O computador jogou tesoura\nVoce ganhou!")
+		rodada++; 
+	}	
+
+	else if(jogadaPlayer == 3 && jogadaCpu == 1) { //player joga tesoura cpu joga papel
+		console.log("O computador jogou papel\nVoce ganhou!"); 
+		rodada++; 
+	}
+	else if(jogadaPlayer === jogadaCpu) { 
+		console.log("O computador jogou "+ dicionario[jogadaCpu] + "\nA rodada empatou!"); 
+	}
+
+	else { 
+		console.log("O computador jogou "+ dicionario[jogadaCpu] + "\nVoce perdeu! a sua pontuacao foi de " + rodada); 	
+		jogadorPerdeu = true;
+	}	
+
+}
+
+
